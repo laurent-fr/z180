9a220c8b1829b2d1852478bd09175341ea613e59
Laurent <laurent@zextras.fr>
2020-01-08 13:01:35 +0100

    font file

--- hardware/vdu/fonts/convert.c
+++ hardware/vdu/fonts/convert.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+char * fontlist[] = {
+"Bm437_PhoenixEGA_8x16.FON",
+"Bm437_Verite_8x16.FON",
+"Bm437_ATI_8x16.FON",
+"Bm437_ATT_PC6300.FON",
+"Bm437_IBM_ISO8.FON",
+"Bm437_CompaqThin_8x16.FON",
+"Bm437_ToshibaLCD_8x16.FON",
+"Bm437_IBM_PS2thin1.FON",
+};
+
+
+void convert(FILE *fpout,char *fontfile);
+
 void main() {
 
-    FILE *fp,*fpout;
+    int i;
+    FILE *fpout;
+
+    fpout=fopen("font.bin","wb");
+    for(i=0;i<8;i++) {
+        convert(fpout,fontlist[i]);
+    }
+    fclose(fpout);
+
+    exit(0);
+}
+
+void convert(FILE *fpout,char *fontfile) {
+ 
+    FILE *fp;
     int c=0;
     int zerocpt=0;
     int count=0;
@@ -12,7 +41,7 @@ void main() {
     char buffer[4096];
     int i;
 
-    fp=fopen("Bm437_PhoenixEGA_8x16.FON","rb");
+    fp=fopen(fontfile,"rb");
     while(!feof(fp)) {
         oldc=c;
         c=fgetc(fp);
@@ -25,13 +54,12 @@ void main() {
 
             if (zerocpt==16) {
                 fread(&buffer,255*16,1,fp);
-                fpout=fopen("font.bin","wb");
                 for(i=0;i<16;i++) fputc(0,fpout);                
                 fwrite(buffer,255*16,1,fpout);
-                fclose(fpout);
-                exit(0);
+                return;
             }
         }
     }
 
 }
+
